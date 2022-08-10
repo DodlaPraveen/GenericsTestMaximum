5b8d97cbfbee8001e6b1153a42ebb2bf012d5621
praveen dodla <dodlapraveen09@gmail.com>
2022-08-10 17:52:46 +0530

    Extended The Max Method More 3 perametrs

--- FindMaximumProblemUsingGenerics/FindMaximumProblemUsingGenerics/Program.cs
+++ FindMaximumProblemUsingGenerics/FindMaximumProblemUsingGenerics/Program.cs
@@ -2,22 +2,13 @@
 
 namespace FindMaximumProblemUsingGenerics
 {
-    class GenericClass<T>
-    {
-        public GenericClass(T msg)
-        {
-            Console.WriteLine(msg);
-            Console.ReadLine();
-        }
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            GenericClass<string> gen = new GenericClass<string>("This is generic class");
-            GenericClass<int> genI = new GenericClass<int>(101);
-            GenericClass<char> getCh = new GenericClass<char>('I');
+            MaxNumbers.maxNumbers();
+            Console.ReadKey();
+
         }
     }
 }
